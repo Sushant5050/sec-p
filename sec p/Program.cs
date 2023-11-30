aa110200775b2aa898785daf2df679400baae575
Sushant5050 <148970856+Sushant5050@users.noreply.github.com>
2023-11-30 14:18:53 +0530

    j

---
+++ sec p/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sec_p
+{
+    internal class Program
+    {
+        public void square()
+        {
+            int a = 10, b;
+            b = a * a;
+            Console.WriteLine("square=" + b);
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.square();
+            Console.ReadLine();
+        }
+    }
+}
